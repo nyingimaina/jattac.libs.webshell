21aa5e79ed909b4c42007b8d3e27caa7fdfcc138
= <=>
2025-01-19 18:25:27 +0300

    added onDefault listener to allow hitting of keys to trigger actions

--- src/WebShell/Buttons/WebShellButton.tsx
+++ src/WebShell/Buttons/WebShellButton.tsx
@@ -8,11 +8,36 @@ interface WebShellButtonProps {
   disabled?: boolean;
   children: React.ReactNode;
   className?: string; // Allow users to pass their own styles
+  isDefault?: boolean;
 }
 
 export class WebShellButton extends Component<WebShellButtonProps> {
+  componentDidMount() {
+    if (this.props.isDefault) {
+      // Add the event listener for Enter key when the component is mounted
+      document.addEventListener('keydown', this.handleKeyDown);
+    }
+  }
+
+  componentWillUnmount() {
+    // Clean up by removing the event listener when the component is unmounted
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    const { isDefault, onClick, disabled } = this.props;
+
+    // If the Enter key is pressed and the button is enabled and isDefault is true
+    if (isDefault && event.key === 'Enter' && !disabled) {
+      event.preventDefault(); // Prevent default action (form submission if inside a form)
+      if (onClick) {
+        onClick(); // Trigger the button's onClick handler
+      }
+    }
+  };
+
   render() {
-    const { buttonType, onClick, disabled, children, className } = this.props;
+    const { buttonType, disabled, children, className } = this.props;
 
     // Determine the button class based on the buttonType prop
     const buttonClass = `${styles.formButton} ${
